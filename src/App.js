5766c923367c88392f5f83e4b07abf2cc39ad926
Tony Spiro <tspiro@tonyspiro.com>
2019-02-09 21:52:07 -0800

    import wordpress posts

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  getParam(param) {
-    var urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(param);
+import React, { useState } from 'react'
+import { Container, Button, Loader, Form } from 'semantic-ui-react'
+import axios from 'axios'
+const importPosts = (e) => {
+  e.preventDefault()
+  const bucket_slug = getParam('bucket_slug')
+  const read_key = getParam('read_key')
+  const write_key = getParam('write_key')
+  const wordpress_url = e.target.elements.wordpress_url.value
+  const number_posts = e.target.elements.number_posts.value
+  const data = {
+    bucket_slug,
+    read_key,
+    write_key,
+    wordpress_url,
+    number_posts
   }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p>When uploaded to your Cosmic JS Bucket, these keys are available in the URL:</p>
-          <p>Bucket Slug: <code style={{ color: '#00AFD7' }}>{this.getParam('bucket_slug')}</code></p>
-          <p>Bucket Read Key: <code style={{ color: '#00AFD7' }}>{this.getParam('read_key')}</code></p>
-          <p>Bucket Write Key: <code style={{ color: '#00AFD7' }}>{this.getParam('write_key')}</code></p>
-        </header>
-      </div>
-    );
-  }
+  axios.post('http://localhost:3000/import-wordpress-posts', data)
+  .then(response => {
+    console.log(response)
+  })
+  .catch(error => {
+    console.log(error)
+  })
 }
-
-export default App;
+const getParam = (param) => {
+  var urlParams = new URLSearchParams(window.location.search)
+  return urlParams.get(param)
+}
+const App = () => {
+  const [loading, setLoading] = useState(false)
+  return (
+    <Container style={{ marginBottom: 100 }}>
+      <h1 style={{marginTop: 20}}>WordPress Import Extension</h1>
+      <p>Follow the steps below to add your WordPress posts to your Cosmic JS Bucket.</p>
+      <h2>Step 1:</h2>
+      <p>Add your WordPress blog feed URL. You should be able to see the RSS feed at <code>http://yourwebsite.com/feed</code></p>
+      <Form style={{ width: '50%'}} onSubmit={ importPosts.bind(this) }>
+        <Form.Field>
+          <label>Your WordPress blog feed URL</label>
+          <input placeholder='http://yourwebsite.com/feed' name='wordpress_url'/>
+        </Form.Field>
+        <h2>Step 2:</h2>
+        <p>How many posts do you want to import?</p>
+        <Form.Field>
+          <label>Number of posts</label>
+          <input placeholder='100' type='number' name='number_posts' />
+        </Form.Field>
+        <h2>Step 3:</h2>
+        <p>Import into your Cosmic JS Bucket.</p>
+        <Button type='submit' onClick={() => setLoading(true)}>{ !loading ? 'Import into Cosmic JS' : 'Loading...'}</Button>
+      </Form>
+    </Container>
+  )
+}
+export default App
