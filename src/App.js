e9d04db301cba5229d17327495b48badbf119381
Tony Spiro <tspiro@tonyspiro.com>
2019-02-12 13:28:09 -0800

    include fail modal on error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
-import { Grid, Modal, Message, Icon, Header, Dimmer, Container, Button, Form, Select } from 'semantic-ui-react'
+import { Grid, Modal, Message, Icon, Header, Dimmer, Container, Button, Form } from 'semantic-ui-react'
 import axios from 'axios'
 import wp_logo from './wp-logo.png'
 import cosmic_logo from './cosmic-logo.svg'
 import './index.css';
+const WP_IMPORT_URL = 'https://wordpress-import-app.cosmicapp1.co/import-wordpress-posts'
+// const WP_IMPORT_URL = 'http://localhost:3000/import-wordpress-posts'
 const getParam = (param) => {
   var urlParams = new URLSearchParams(window.location.search)
   return urlParams.get(param)
@@ -27,7 +29,7 @@ const App = () => {
       number_posts
     }
     setLoading(true)
-    axios.post('https://wordpress-import-app.cosmicapp1.co/import-wordpress-posts', data)
+    axios.post(WP_IMPORT_URL, data)
     .then(response => {
       console.log(response)
       setLoading(false)
@@ -36,25 +38,27 @@ const App = () => {
     .catch(error => {
       console.log(error)
       setLoading(false)
+      setShowFailModal(true)
     })
   }
   const [loading, setLoading] = useState(false)
   const [show_success_modal, setShowSuccessModal] = useState(false)
+  const [show_fail_modal, setShowFailModal] = useState(false)
   if (loading)
     return (
       <Dimmer active>
         <h2 style={{ marginBottom: 60 }}>Importing your posts...</h2>
         <Grid columns='three'>
           <Grid.Column>
-            <div class="card-floating" style={{ width: 180, height: 180 }}><img style={{ width: '100%' }} src={wp_logo} /></div>
+            <div className="card-floating" style={{ width: 180, height: 180 }}><img alt="WordPress Logo" style={{ width: '100%' }} src={wp_logo} /></div>
           </Grid.Column>
           <Grid.Column style={{paddingTop: 50, textAlign: 'center'}}>
-            <svg class="objects-import" width="486" height="468" viewBox="0 0 486 418" fill="none">
+            <svg className="objects-import" width="486" height="468" viewBox="0 0 486 418" fill="none">
               <path d="M225 6673V38" stroke="#00AFD7" stroke-opacity=".4" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="5 15"></path>
             </svg>
           </Grid.Column>
           <Grid.Column>
-          <div class="card-floating" style={{ width: 180, height: 180 }}><img style={{ width: '100%' }} src={cosmic_logo} /></div>
+          <div className="card-floating" style={{ width: 180, height: 180 }}><img alt="Cosmic JS Logo" style={{ width: '100%' }} src={cosmic_logo} /></div>
           </Grid.Column>
         </Grid>
       </Dimmer>
@@ -65,7 +69,7 @@ const App = () => {
         <Header icon='check' color='green' content='Posts Imported' />
         <Modal.Content>
           <p style={{ fontSize: 16 }}>
-            Your posts have been successfully imported!&nbsp;&nbsp;&nbsp;<a href={`https://cosmicjs.com/${bucket_slug}/objects?type=posts`} target='_parent'>Go see them&nbsp;&nbsp;<Icon name='external link' /></a>
+            Your posts have been successfully imported!&nbsp;&nbsp;&nbsp;<a href={`https://cosmicjs.com/${bucket_slug}/objects?type=posts`} target='_parent'>Go see them&nbsp;&nbsp;<Icon name='external' /></a>
           </p>
         </Modal.Content>
         <Modal.Actions>
@@ -75,30 +79,43 @@ const App = () => {
         </Modal.Actions>
       </Modal>
     )
+  if (show_fail_modal)
+    return (
+      <Modal open={true} basic size='small'>
+        <Header icon='times' color='red' content='Posts Not Imported' />
+        <Modal.Content>
+          <p style={{ fontSize: 16 }}>
+            There was an error with this request. Make sure the URL is a correct URL with the WordPress RSS feed. 
+            For example: https://techcrunch.com/feed&nbsp;&nbsp;<a href="https://techcrunch.com/feed" target="_blank" rel="noopener noreferrer"><Icon name="external" /></a>
+          </p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button inverted onClick={() => setShowFailModal(false)}>
+            <Icon name='times' /> Close
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    )
   
   const num_options = []
-  let i = 0
-  while(i <= 10) {
-    num_options.push({
-      key: i * 20,
-      value: i * 20,
-      text: i * 20,
-    })
-    i++
+  let i = 20
+  while(i <= 200) {
+    num_options.push(i)
+    i+=20
   }
   return (
     <Container style={{ marginBottom: 100, paddingTop: 20 }}>
       <h1>WordPress Import</h1>
       <div style={{ marginBottom: 20 }}>
-        <img style={{ width: 250, height: 116 }} src='https://cosmic-s3.imgix.net/31cc7ee0-2d80-11e9-9636-75201e82cc8c-wordpress-to-cosmic.jpg?w=500' />
+        <img alt="WordPress to Cosmic" style={{ width: 250, height: 116 }} src='https://cosmic-s3.imgix.net/31cc7ee0-2d80-11e9-9636-75201e82cc8c-wordpress-to-cosmic.jpg?w=500' />
         { /* Hidden images loaded in bg */ }
-        <img src={wp_logo} style={{ width: 0, height: 0 }}/>
-        <img src={cosmic_logo} style={{ width: 0, height: 0 }}/>
+        <img alt="WordPress Logo" src={wp_logo} style={{ width: 0, height: 0 }}/>
+        <img alt="Cosmic JS Logo" src={cosmic_logo} style={{ width: 0, height: 0 }}/>
       </div>
       <p>Follow the steps below to add your WordPress posts to your Cosmic JS Bucket.</p>
       <h2>Step 1:</h2>
       <p>Add your WordPress blog feed URL. You should be able to see the RSS feed at <br /><code>http://yourwebsite.com/feed</code></p>
-      <p>Or try a working sample URL: <br /><code>https://techcrunch.com/feed</code></p>
+      <p>Or copy this working URL: <br /><code>https://techcrunch.com/feed</code>&nbsp;&nbsp;<a href="https://techcrunch.com/feed" target="_blank" rel="noopener noreferrer"><Icon name="external" /></a></p>
       <Form onSubmit={ importPosts.bind(this) }>
         <Form.Field>
           <label>Your WordPress blog feed URL</label>
@@ -110,8 +127,8 @@ const App = () => {
           <label>Number of posts</label>
           <select name='number_posts' style={{ width: 100 }}>
             {
-              num_options.map(option => {
-                return <option value={option.value}>{option.value}</option>
+              num_options.map(num => {
+                return <option key={num} value={num}>{num}</option>
               })
             }
           </select>
